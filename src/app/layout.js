37d7e5388cfc1836e609682323f0d95d912fe39f
Gamseng M Marak <47160012+GamsengMazee@users.noreply.github.com>
2025-06-13 11:40:26 +0530

    first commit

---
+++ src/app/layout.js
@@ -0,0 +1,47 @@
+import { Geist, Geist_Mono } from "next/font/google";
+import "./globals.css";
+import Header from "@/components/Header";
+
+const geistSans = Geist({
+  variable: "--font-geist-sans",
+  subsets: ["latin"],
+});
+
+const geistMono = Geist_Mono({
+  variable: "--font-geist-mono",
+  subsets: ["latin"],
+});
+
+export const metadata = {
+  title: "IMDB Clone",
+  description: "A clone of the IMDB website built with Next.js",
+  openGraph: {
+    title: "IMDB Clone",
+    description: "A clone of the IMDB website built with Next.js",
+    url: "https://imdb-clone.vercel.app",
+    siteName: "IMDB Clone",
+    images: [
+      {
+        url: "https://imdb-clone.vercel.app/og-image.png",
+        width: 1200,
+        height: 630,
+        alt: "IMDB Clone OG Image",
+      },
+    ],
+    locale: "en_US",
+    type: "website",
+  },
+};
+
+export default function RootLayout({ children }) {
+  return (
+    <html lang="en">
+      <body
+        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
+      >
+        <Header />
+        {children}
+      </body>
+    </html>
+  );
+}
