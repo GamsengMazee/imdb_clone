055aafeb30803249faf76648ab6f47f8ecf0ea35
Gamseng M Marak <47160012+GamsengMazee@users.noreply.github.com>
2025-06-13 17:55:41 +0530

    Created Navbar Component

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,7 +1,8 @@
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
-import Header from "@/components/Header";
+import Header from "../components/Header";
 import Providers from "./Providers";
+import Navbar from "../components/Navbar";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -42,6 +43,7 @@ export default function RootLayout({ children }) {
       >
         <Providers>
           <Header />
+          <Navbar />
           {children}
         </Providers>
       </body>
